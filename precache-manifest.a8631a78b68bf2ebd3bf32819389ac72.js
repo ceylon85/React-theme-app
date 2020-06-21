a31b704b7fb987b13731a512607bfa15463be14b
ceylon85 <canleez123@gmail.com>
2020-06-21 23:56:19 +0900

    Updates

---
+++ precache-manifest.a8631a78b68bf2ebd3bf32819389ac72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c482285a1770fa53fbee603e98be31e6",
+    "url": "/React-theme-app/index.html"
+  },
+  {
+    "revision": "94cabb9ef74fb5c7535c",
+    "url": "/React-theme-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b051b5c1da4dfc0a8912",
+    "url": "/React-theme-app/static/js/2.868f722e.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/React-theme-app/static/js/2.868f722e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cabb9ef74fb5c7535c",
+    "url": "/React-theme-app/static/js/main.bc17ac92.chunk.js"
+  },
+  {
+    "revision": "d9aee1c86b7cb8d7082a",
+    "url": "/React-theme-app/static/js/runtime-main.6edfab85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-theme-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
